a4ecb3594255b9f0e1fc157cb8facd602d8308bf
dx3mod <dx3mod@bk.ru>
2024-07-16 23:34:59 +0300

    Add pseudo tests

---
+++ test/test_rpmfile.ml
@@ -0,0 +1,20 @@
+module Rpm_reader = Rpmfile.Reader (Rpmfile.Selector.All)
+
+let metadata = Rpm_reader.of_file_exn "hello-2.12.1-1.7.x86_64.rpm"
+
+let test_name () =
+  Alcotest.(check string) "name" "hello" (Rpmfile.name metadata)
+
+let test_version () =
+  Alcotest.(check @@ pair int int) "lead version" (3, 0) metadata.lead.version
+
+let () =
+  let open Alcotest in
+  run "Rpmfile"
+    [
+      ( "package",
+        [
+          test_case "name" `Quick test_name;
+          test_case "version" `Quick test_version;
+        ] );
+    ]
