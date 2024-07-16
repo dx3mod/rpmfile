e3b688c2e7c2d64a192da282db929467e575be15
dx3mod <dx3mod@bk.ru>
2024-07-16 15:36:31 +0300

    Add reader interface

--- lib/rpmfile.ml
+++ lib/rpmfile.ml
@@ -1,1 +1,29 @@
 type lead = Lead.t
+
+type header = (tag * value) list
+and tag = int
+and value = Header.index_value
+
+type metadata = { lead : lead; signature : header; header : header }
+
+(* Parser *)
+module P = struct
+  let metadata_parser =
+    let open Angstrom in
+    let* lead = Lead.parser in
+    let* signature = Header.parser in
+    let* header = Header.parser in
+
+    return { lead; signature; header }
+end
+
+module Reader = struct
+  type result = (metadata, string) Stdlib.result
+
+  let of_string =
+    Angstrom.(parse_string ~consume:Consume.Prefix) P.metadata_parser
+
+  let of_file path =
+    In_channel.with_open_bin path (fun ic ->
+        In_channel.input_all ic |> of_string)
+end
