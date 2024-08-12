97907437a91b2ae54cc9cbf95f1e3e58c8150de7
dx3mod <dx3mod@bk.ru>
2024-08-12 22:34:41 +0300

    Fix package names

--- eio/tests/test_select_all.ml
+++ eio/tests/test_select_all.ml
@@ -3,7 +3,7 @@ module Rpm_reader = Rpmfile_eio.Reader.Make (Rpmfile.Selector.All)
 let metadata =
   Eio_main.run (fun env ->
       let path =
-        Eio.Path.(env#fs / "../../test_data/hello-2.12.1-1.7.x86_64.rpm")
+        Eio.Path.(env#fs / "../../test_misc/hello-2.12.1-1.7.x86_64.rpm")
       in
       Eio.Path.with_open_in path (Rpm_reader.of_flow ~max_size:10_000))
 
--- unix/tests/test_select_base.ml
+++ unix/tests/test_select_base.ml
@@ -1,18 +1,18 @@
-module Rpm_reader = Rpmfile_unix.Reader.Make (Rpmfile.Selector.All)
+module Rpm_reader = Rpmfile_unix.Reader.Make (Rpmfile.Selector.Base)
 
 let metadata =
-  Rpm_reader.of_file "../../test_data/hello-2.12.1-1.7.x86_64.rpm"
+  Rpm_reader.of_file "../../test_misc/hello-2.12.1-1.7.x86_64.rpm"
   |> Unwrap.unwrap
 
 let () =
   let open Alcotest in
   let open Testlib in
-  run "Rpmfile_unix (Selector.All)"
+  run "Rpmfile_unix (Selector.Base)"
     [
       ( "hello-2.12.1-1.7.x86_64.rpm",
         [
           test_case "name" `Quick (test_name metadata);
           test_case "version" `Quick (test_version metadata);
-          test_case "filenames" `Quick (test_array_string metadata);
+          test_case "filenames" `Quick (test_not_found metadata);
         ] );
     ]
